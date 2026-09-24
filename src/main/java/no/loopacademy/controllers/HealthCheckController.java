235e781f353b6a63d011172241a760152fafbe16
Tobias Friis <140440078+TobiasFriis@users.noreply.github.com>
2026-09-24 14:09:01 +0200

    UPDATE: remove unused imports

--- src/main/java/no/loopacademy/controllers/HealthCheckController.java
+++ src/main/java/no/loopacademy/controllers/HealthCheckController.java
@@ -3,20 +3,15 @@ package no.loopacademy.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse;
-import java.util.Map;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-
-@RestController 
+@RestController
 @RequestMapping("/api/health")
 public class HealthCheckController {
 
-    @GetMapping
-    public ResponseEntity<Void> getHealthCheck() {
-        return ResponseEntity.ok().build();
-    }
+  @GetMapping
+  public ResponseEntity<Void> getHealthCheck() {
+    return ResponseEntity.ok().build();
+  }
 }
