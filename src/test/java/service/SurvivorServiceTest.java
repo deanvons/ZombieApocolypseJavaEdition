665ad821bb90cd2d801815c7796105b464bcb77e
leahbeathe <leahbeathe@gmail.com>
2026-09-25 13:14:38 +0200

    CHORE: Added remaining tests for SurvivorService

--- src/test/java/service/SurvivorServiceTest.java
+++ src/test/java/service/SurvivorServiceTest.java
@@ -2,17 +2,22 @@ package service;
 
 
 import no.loopacademy.exceptions.SurvivorNotFoundException;
+import no.loopacademy.models.survivors.CareGiver;
 import no.loopacademy.models.survivors.Survivor;
+import no.loopacademy.models.survivors.SurvivorTypes;
 import no.loopacademy.services.SurvivorService;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SurvivorServiceTest {
     @Test
-    public void SurvivorNotFoundTest_shouldThrowException() {
+    public void testSurvivorNotFound_shouldThrowException() {
 
         SurvivorService survivorService = new SurvivorService();
 
@@ -22,14 +27,36 @@ public class SurvivorServiceTest {
     }
 
     @Test
-    void create() {
+    void testCreateSurvivorWithCorrectType_checksCaregiverType_shouldPass() {
+        CareGiver careGiver = new CareGiver("Tester");
+        SurvivorService survivorService = new SurvivorService();
+
+        Survivor survivor = survivorService.create("Tester2", SurvivorTypes.CAREGIVER);
+
+        assertEquals(careGiver.getClass(), survivor.getClass());
     }
 
     @Test
-    void findAll() {
+    void testFindAllSurvivors_shouldReturnAllSurvivors_shouldPass() {
+        SurvivorService survivorService = new SurvivorService();
+        Survivor survivor1 = survivorService.create("Tester1", SurvivorTypes.CAREGIVER);
+        Survivor survivor2 = survivorService.create("Tester2", SurvivorTypes.CAREGIVER);
+        List<Survivor> expectedSurvivors = List.of(survivor1, survivor2);
+
+        List<Survivor> actualsurvivors =  survivorService.findAll();
+
+        assertEquals(expectedSurvivors, actualsurvivors);
+
     }
 
     @Test
-    void findById() {
+    void testFindSurvivorById_passingSurvivorId_shouldReturnSurvivorById_shouldPass() {
+        SurvivorService survivorService = new SurvivorService();
+        Survivor expectedSurvivor = survivorService.create("Tester1", SurvivorTypes.CAREGIVER);
+        expectedSurvivor.setId(1L);
+
+        Survivor actualSurvivor = survivorService.findById(1L);
+
+        assertEquals(expectedSurvivor, actualSurvivor);
     }
 }
