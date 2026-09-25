25ccbc8ea792f837b4174e6545713084768f5040
Dr. Dean von Schoultz <drdean@loopacademy.no>
2026-09-25 10:32:16 +0200

    CHORE: CICD test

--- src/main/java/no/loopacademy/controllers/HealthCheckController.java
+++ src/main/java/no/loopacademy/controllers/HealthCheckController.java
@@ -1,10 +1,9 @@
 package no.loopacademy.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/health")
@@ -16,7 +15,7 @@ public class HealthCheckController {
   }
 
     @GetMapping("new")
-  public ResponseEntity<Void> getHealthCheck() {
+  public ResponseEntity<Void> getHealthCheckNew() {
     return ResponseEntity.ok().build();
   }
 }
