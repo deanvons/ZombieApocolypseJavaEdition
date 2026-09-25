00e64593c551f013c8c14a87c057576d9555f5d7
Tobias Friis <140440078+TobiasFriis@users.noreply.github.com>
2026-09-25 13:29:46 +0200

    FEAT: add and remove skills methods to survivor service with unit tests

--- src/main/java/no/loopacademy/services/SurvivorService.java
+++ src/main/java/no/loopacademy/services/SurvivorService.java
@@ -1,12 +1,13 @@
 package no.loopacademy.services;
 
-
 import no.loopacademy.exceptions.SurvivorNotFoundException;
+import no.loopacademy.models.skills.Skill;
 import no.loopacademy.models.survivors.CareGiver;
 import no.loopacademy.models.survivors.Survivor;
 import no.loopacademy.models.survivors.SurvivorTypes;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +17,8 @@ public class SurvivorService {
 
     Map<Long, Survivor> survivors = new HashMap<Long, Survivor>();
 
-    public Survivor create(String name, SurvivorTypes type){
-          Survivor survivor=  switch(type){
+    public Survivor create(String name, SurvivorTypes type) {
+        Survivor survivor = switch (type) {
             case CAREGIVER -> new CareGiver(name);
             default -> null;
         };
@@ -30,16 +31,33 @@ public class SurvivorService {
         return survivor;
     }
 
-    public List<Survivor> findAll(){
+    public List<Survivor> findAll() {
         return List.copyOf(survivors.values());
     }
 
-    public Survivor findById(Long id){
-        try{
+    public Survivor findById(Long id) {
+        try {
             return survivors.get(id);
-        } catch(NullPointerException e){
-            throw new SurvivorNotFoundException("Survivor with id:"+id+" Not Found");
+        } catch (NullPointerException e) {
+            throw new SurvivorNotFoundException("Survivor with id:" + id + " Not Found");
         }
+    }
+
+    public void addSkill(Long id, Skill skill) {
+        Survivor survivor = findById(id);
+        List<Skill> skills = new ArrayList<>(survivor.getSkills());
+        if (skills.contains(skill)) {
+            return;
+        }
+        skills.add(skill);
+        survivor.setSkills(skills);
+    }
+
+    public void removeSkill(Long id, Skill skill) {
+        Survivor survivor = findById(id);
+        List<Skill> skills = new ArrayList<>(survivor.getSkills());
+        skills.remove(skill);
+        survivor.setSkills(skills);
     }
 
 }
--- src/test/java/service/SurvivorServiceTest.java
+++ src/test/java/service/SurvivorServiceTest.java
@@ -1,8 +1,53 @@
 package service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import no.loopacademy.models.skills.Skill;
+import no.loopacademy.models.survivors.SurvivorTypes;
+import no.loopacademy.services.SurvivorService;
 
 public class SurvivorServiceTest {
+
+    @Test
+    void survivorService_addSkillToSurvivor_survivorWithNewSkill() {
+        SurvivorService survivorService = new SurvivorService();
+        survivorService.create("CareGiver", SurvivorTypes.CAREGIVER);
+        List<Skill> expectedOutput = new ArrayList<>(List.of(Skill.FieldMedicine, Skill.PsychologicalSupport));
+        expectedOutput.add(Skill.Accuracy);
+
+        survivorService.addSkill(1L, Skill.Accuracy);
+        List<Skill> actualOutput = survivorService.findById(1L).getSkills();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void survivorService_addSkillToSurvivorThatItAlreadyHas_noChangeInSurvivorSkills() {
+        SurvivorService survivorService = new SurvivorService();
+        survivorService.create("CareGiver", SurvivorTypes.CAREGIVER);
+        List<Skill> expectedOutput = new ArrayList<>(List.of(Skill.FieldMedicine, Skill.PsychologicalSupport));
+
+        survivorService.addSkill(1L, Skill.FieldMedicine);
+        List<Skill> actualOutput = survivorService.findById(1L).getSkills();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void survivorService_removeSkillFromSurvivor_survivorWithoutSkill() {
+        SurvivorService survivorService = new SurvivorService();
+        survivorService.create("CareGiver", SurvivorTypes.CAREGIVER);
+        List<Skill> expectedOutput = new ArrayList<>(List.of(Skill.FieldMedicine));
+
+        survivorService.removeSkill(1L, Skill.PsychologicalSupport);
+        List<Skill> actualOutput = survivorService.findById(1L).getSkills();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
 }
