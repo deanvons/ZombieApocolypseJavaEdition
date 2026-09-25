14557bb13b9f1a9830811685cfa25b6733586e36
leahbeathe <leahbeathe@gmail.com>
2026-09-25 15:39:18 +0200

    CHORE: GET/POST /api/survivors, GET /api/survivors/{id}

--- src/main/java/no/loopacademy/controllers/SurvivorController.java
+++ src/main/java/no/loopacademy/controllers/SurvivorController.java
@@ -1,10 +1,35 @@
 package no.loopacademy.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import no.loopacademy.models.survivors.Survivor;
+import no.loopacademy.models.survivors.SurvivorTypes;
+import no.loopacademy.services.SurvivorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/survivors")
 public class SurvivorController {
+
+    @Autowired
+    private SurvivorService survivorService;
+
+    @GetMapping
+    public List<Survivor> getSurvivors() {
+        return survivorService.findAll();
+    }
+
+    @PostMapping
+    public Survivor createSurvivor(@RequestBody String name, @RequestBody SurvivorTypes survivorType) {
+        return survivorService.create(name, survivorType);
+    }
+
+    @GetMapping("/{id}")
+    public Survivor getSurvivorById(@PathVariable Long id) {
+        return survivorService.findById(id);
+    }
+
+
     
 }
